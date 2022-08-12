d29f9e70aa2b13896e092ee32bfafc8d7c98eaef
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-08-12 21:43:11 +0700

    Hash Map

---
+++ bin/hash_map.dart
@@ -0,0 +1,17 @@
+import 'dart:collection';
+
+void main(){
+
+  final scores = HashMap<String, int>();
+
+  scores["Eko"] = 100;
+  scores["Budi"] = 100;
+  scores["Joko"] = 100;
+  scores["Dimas"] = 100;
+  scores["Donis"] = 100;
+  scores["Hendra"] = 100;
+  scores["Andi"] = 100;
+
+  print(scores);
+
+}

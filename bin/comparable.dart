a58df41e2aec8f5bc1682d4f8eafed900b2c6bac
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-08-12 14:22:11 +0700

    Comparable

---
+++ bin/comparable.dart
@@ -0,0 +1,29 @@
+import 'dart:collection';
+
+class Category implements Comparable<Category>{
+  String id;
+  String name;
+
+  Category(this.id, this.name);
+
+  @override
+  int compareTo(Category other) {
+    return id.compareTo(other.id);
+  }
+
+  @override
+  String toString() {
+    return "Category{id: $id, name: $name}";
+  }
+}
+
+void main(){
+
+  final treeSet = SplayTreeSet<Category>();
+  treeSet.add(Category('3', 'Category 3'));
+  treeSet.add(Category('1', 'Category 1'));
+  treeSet.add(Category('2', 'Category 2'));
+
+  print(treeSet);
+
+}

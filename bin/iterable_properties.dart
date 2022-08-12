4ee9b3f15648d5f9aa4404d2e721af43bac7d222
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-08-12 16:27:06 +0700

    Iterable Properties

---
+++ bin/iterable_properties.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  final numbers = [1,2,3,4,5,6,7,8,9,10];
+
+  print(numbers.length);
+  print(numbers.first);
+  print(numbers.last);
+  print(numbers.isEmpty);
+  print(numbers.isNotEmpty);
+  // print(numbers.single);
+
+}

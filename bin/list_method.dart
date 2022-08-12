6ba51d9d2b58786e789aba0dc6f5f7189b4a68f0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-08-12 16:46:44 +0700

    List Method

---
+++ bin/list_method.dart
@@ -0,0 +1,11 @@
+void main(){
+
+  final names = ["Eko", "Khannedy"];
+
+  print(names);
+
+  names.insert(1, "Kurniawan");
+
+  print(names);
+
+}
---
+++ bin/list_operator.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  final names = ["Eko", "Kurniawan", "KHannedy"];
+  final authors = ["Programmer", "Zaman", "Now"];
+
+  final combine = names + authors;
+
+  print(names);
+  print(authors);
+  print(combine);
+
+}
